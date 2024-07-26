ab0f73a7998ff77d58ee4e96d12dbcfc3e8b7681
Derek Huang <djh458@stern.nyu.edu>
2024-07-26 00:16:54 -0400

    common.h: add some commonly (reused) stuff

---
+++ include/npygl/common.h
@@ -0,0 +1,35 @@
+/**
+ * @file common.g
+ * @author Derek Huang
+ * @brief C/C++ header for common shared macro
+ * @copyright MIT License
+ */
+
+#ifndef NPYGL_COMMON_H_
+#define NPYGL_COMMON_H_
+
+/**
+ * C++ version macro that works with MSVC.
+ *
+ * This allows us to avoid using `/Zc:__cplusplus` during compilation.
+ */
+#if defined(__cplusplus)
+// MSVC
+#if defined(_MSVC_LANG)
+#define NPYGL_CPLUSPLUS _MSVC_LANG
+#else
+#define NPYGL_CPLUSPLUS __cplusplus
+#endif  // !defined(_MSVC_LANG)
+#endif  // !defined(__cplusplus)
+
+/**
+ * Stringify argument without macro expansion.
+ */
+#define NPYGL_STRINGIFY_I(x)
+
+/**
+ * Stringify argument with macro expansion.
+ */
+#define NPYGL_STRINGIY(x) NPYGL_STRINGIFY_I(x)
+
+#endif  // NPYGL_COMMON_H_
