44fe5b3ecd8493bd3f0dbaf22c7d6fa3c94d8a04
Derek Huang <djh458@stern.nyu.edu>
2024-07-30 03:53:11 -0400

    npy_helpers_test.cc: add sine() function to test flat view

--- test/npy_helpers_test.cc
+++ test/npy_helpers_test.cc
@@ -8,12 +8,27 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+#include <cmath>
 #include <cstdlib>
 #include <ios>
 #include <iostream>
 
 #include "npygl/py_helpers.hh"
 #include "npygl/npy_helpers.hh"
+
+namespace {
+/**
+ * Compute the sine of the view elements.
+ *
+ * @param view NumPy array view
+ */
+void sine(const npygl::ndarray_flat_view<double>& view)
+{
+  for (decltype(view.size()) i = 0; i < view.size(); i++)
+    view[i] = std::sin(view[i]);
+}
+
+}  // namespace
 
 int main()
 {
@@ -47,5 +62,10 @@ int main()
   auto ar = res.as<PyArrayObject>();
   std::cout << "Is double type? " << npygl::is_type<double>(ar) << std::endl;
   std::cout << "Is behaved? " << PyArray_ISBEHAVED(ar) << std::endl;
+  // apply sine function to NumPy array and print it again
+  sine(ar);
+  npygl::py_print(res);
+  npygl::py_error_exit();
+  std::cout << std::endl;
   return EXIT_SUCCESS;
 }
