3acdb4700e537c4308f305e6eff24ae23c15fec4
Derek Huang <djh458@stern.nyu.edu>
2024-08-08 00:36:22 -0400

    range_views_test.cc: use type aliases for convenience, don't nest array_wrapper template, make flt_input_3 have unsigned int as value type

--- test/npygl_test/range_views_test.cc
+++ test/npygl_test/range_views_test.cc
@@ -22,7 +22,11 @@ namespace {
  * `FlatViewTest` input for a `std::vector<float>`.
  */
 struct flt_input_1 {
-  std::vector<float> input() const
+  using value_type = float;
+  using container_type = std::vector<value_type>;
+  using view_type = npygl::flat_view<value_type>;
+
+  container_type input() const
   {
 NPYGL_MSVC_WARNING_PUSH()
 NPYGL_MSVC_WARNING_DISABLE(4305)
@@ -30,12 +34,12 @@ NPYGL_MSVC_WARNING_DISABLE(4305)
 NPYGL_MSVC_WARNING_POP()
   }
 
-  auto operator()(float x) const noexcept
+  auto operator()(value_type x) const noexcept
   {
     return std::sin(x);
   }
 
-  npygl::flat_view<float> view(std::vector<float>& values) const noexcept
+  view_type view(container_type& values) const noexcept
   {
     return {values.data(), values.size()};
   }
@@ -45,102 +49,113 @@ NPYGL_MSVC_WARNING_POP()
  * `FlatViewTest` input for a `std::vector<double>`.
  */
 struct flt_input_2 {
-  std::vector<double> input() const
+  using value_type = double;
+  using container_type = std::vector<value_type>;
+  using view_type = npygl::flat_view<value_type>;
+
+  container_type input() const
   {
     return {0.045, 0.334, 0.12, 6.555};
   }
 
-  auto operator()(double x) const noexcept
+  auto operator()(value_type x) const noexcept
   {
     return std::sqrt(x);
   }
 
-  npygl::flat_view<double> view(std::vector<double>& values) const noexcept
+  view_type view(container_type& values) const noexcept
   {
     return {values.data(), values.size()};
   }
 };
 
 /**
- * `FlatViewTest` input for a static double array wrapper.
+ * Array wrapper template to allow array iteration.
+ *
+ * @tparam T Element type
+ * @tparam N Array size
+ */
+template <typename T, std::size_t N>
+class array_wrapper {
+public:
+  /**
+   * Ctor.
+   *
+   * By decaying a reference to an array to a pointer the `begin()` and
+   * `end()` iterator members can be `const` member functions.
+   */
+  array_wrapper(T (&ar)[N]) noexcept : data_{ar} {}
+
+  /**
+   * Return iterator to beginning of array.
+   */
+  auto begin() const noexcept
+  {
+    return data_;
+  }
+
+  /**
+   * Return iterator to one past end of array.
+   */
+  auto end() const noexcept
+  {
+    return data_ + N;
+  }
+
+  /**
+   * Check if two `array_wrapper` instances are equal.
+   *
+   * This is to support `EXPECT_EQ` usage in Google Test.
+   *
+   * @note This is different than `std::array<T, N>::operator==` which only
+   *  works on arrays that are of the same compile-time length.
+   *
+   * @tparam N_ Size of the other `array_wrapper`
+   */
+  template <std::size_t N_>
+  bool operator==(const array_wrapper<T, N_>& other) const noexcept
+  {
+    // if different sizes, definitely not equal
+    if constexpr (N != N_)
+      return false;
+    // other perform elementwise check
+    else {
+      // same size so no need to track two sizes
+      for (decltype(N) i = 0; i < N; i++)
+        if (*(begin() + i) != *(other.begin() + i))
+          return false;
+      return true;
+    }
+  }
+
+private:
+  T* data_;
+};
+
+
+/**
+ * `FlatViewTest` input for a static unsigned int array wrapper.
  */
 struct flt_input_3 {
-  /**
-   * Nested array wrapper template to allow array iteration.
-   *
-   * @tparam T Element type
-   * @tparam N Array size
-   */
-  template <typename T, std::size_t N>
-  class array_wrapper {
-  public:
-    /**
-     * Ctor.
-     *
-     * By decaying a reference to an array to a pointer the `begin()` and
-     * `end()` iterator members can be `const` member functions.
-     */
-    array_wrapper(T (&ar)[N]) noexcept : data_{ar} {}
-
-    /**
-     * Return iterator to beginning of array.
-     */
-    auto begin() const noexcept
-    {
-      return data_;
-    }
-
-    /**
-     * Return iterator to one past end of array.
-     */
-    auto end() const noexcept
-    {
-      return data_ + N;
-    }
-
-    /**
-     * Check if two `array_wrapper` instances are equal.
-     *
-     * This is to support `EXPECT_EQ` usage in Google Test.
-     *
-     * @tparam N_ Size of the other `array_wrapper`
-     */
-    template <std::size_t N_>
-    bool operator==(const array_wrapper<T, N_>& other) const noexcept
-    {
-      // if different sizes, definitely not equal
-      if constexpr (N != N_)
-        return false;
-      // other perform elementwise check
-      else {
-        // same size so no need to track two sizes
-        for (decltype(N) i = 0; i < N; i++)
-          if (*(begin() + i) != *(other.begin() + i))
-            return false;
-        return true;
-      }
-    }
-
-  private:
-    T* data_;
-  };
+  using value_type = unsigned int;
+  using view_type = npygl::flat_view<value_type>;
 
   auto input() const noexcept
   {
-    static double ar[] = {4.33, 2.344, 1.5151, 8.9};
+    static value_type ar[] = {4U, 1U, 13U, 15U, 888U, 71U, 190U};
     return array_wrapper{ar};
   }
 
-  auto operator()(double x) const noexcept
+  auto operator()(value_type x) const noexcept
   {
-    return 0.5 * x * x + 4 * x + 5;
+    return 2U * x * x + 4U * x + 5;
   }
 
   template <std::size_t N>
-  npygl::flat_view<double> view(array_wrapper<double, N> ar) const noexcept
+  view_type view(array_wrapper<value_type, N> ar) const noexcept
   {
-    // silence warning about narrowing conversion
-    return {ar.begin(), static_cast<std::size_t>(ar.end() - ar.begin())};
+    // cast to silence warning about narrowing conversion (signed to unsigned)
+    return {ar.begin(), static_cast<decltype(N)>(ar.end() - ar.begin())};
   }
 };
 
