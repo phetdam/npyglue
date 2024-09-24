6ab736edce0b31efc27311028effa76a1b2c3837
Derek Huang <djh458@stern.nyu.edu>
2024-09-23 23:59:32 -0400

    py_format_test.cc: add metprogramming helpers to create a test_main() function to run the tests with less boilerplate

--- test/py_format_test.cc
+++ test/py_format_test.cc
@@ -116,35 +116,133 @@ struct format_checker<std::tuple<Ts...>> : format_checker<Ts...> {};
 template <typename... Ts>
 constexpr format_checker<Ts...> check_format{};
 
+/**
+ * Input type holding a format type and the input types.
+ *
+ * @tparam N Length of format string literal
+ * @tparam Ts... Types to use in the format string
+ *
+ * @param fmt Reference to the expected string literal format string
+ */
+template <std::size_t N, typename... Ts>
+struct format_input {
+  using format_types = std::tuple<Ts...>;
+  const char (&fmt)[N];
+};
+
+/**
+ * Partial specialization taking a tuple of input types.
+ *
+ * @tparam N Length of format string literal
+ * @tparam Ts... Types to use in the format string
+ *
+ * @param fmt Reference to the expected string literal format string
+ */
+template <std::size_t N, typename... Ts>
+struct format_input<N, std::tuple<Ts...>> {
+  using format_types = std::tuple<Ts...>;
+  const char (&fmt)[N];
+};
+
+/**
+ * Traits helper to check if several types are `std::tuple`.
+ *
+ * @tparam Ts... types
+ */
+template <typename... Ts>
+struct is_tuple : std::false_type {};
+
+/**
+ * True specialization when the type is a single `std::tuple`.
+ *
+ * @tparam Ts... Tuple types
+ */
+template <typename... Ts>
+struct is_tuple<std::tuple<Ts...>> : std::true_type {};
+
+/**
+ * Recursive specialization when there is more than one `std::tuple`.
+ *
+ * @tparam Ts... Tuple types
+ * @tparam Us... Subsequent types to check
+ */
+template <typename... Ts, typename... Us>
+struct is_tuple<std::tuple<Ts...>, Us...> : is_tuple<Us...> {};
+
+/**
+ * Check if all the types are `std::tuple`.
+ *
+ * @tparam Ts... types
+ */
+template <typename... Ts>
+inline constexpr bool is_tuple_v = is_tuple<Ts...>::value;
+
+/**
+ * Make a `format_input` from a type and a string literal.
+ *
+ * This allows deducing the string literal length from a function parameter.
+ *
+ * @note For convenience of template deduction the
+ *  `format_input<N, std::tuple<Ts...>>` is used when creating the inputs.
+ *
+ * @tparam T Tuple of input types
+ * @tparam N Length of format string literal
+ */
+template <typename T, std::size_t N, typename = std::enable_if_t<is_tuple_v<T>>>
+constexpr auto make_input(const char (&fmt)[N])
+{
+  return format_input<N, T>{fmt};
+}
+
+/**
+ * Test main function.
+ *
+ * @tparam Ns... String literal expected format lengths
+ * @tparam Inputs... Tuples of input types
+ *
+ * @param inputs... Testing inputs
+ * @returns 0 on success, 1 on failure
+ */
+template <std::size_t... Ns, typename... Inputs>
+std::enable_if_t<is_tuple_v<Inputs...>, int>
+test_main(const format_input<Ns, Inputs>&... inputs)
+{
+  // number of inputs/tests + number of failed
+  constexpr auto n_tests = sizeof...(Ns);
+  unsigned int n_failed = 0;
+  // loop through pack to update number of failed
+  (check_format<Inputs>(n_failed, inputs.fmt), ...);
+  // report failure/success
+  if (n_failed)
+    std::cout << "Failed: " << n_failed << " of " << n_tests << std::endl;
+  else
+    std::cout << "Passed " << n_tests << " of " << n_tests << std::endl;
+  return !!n_failed;
+}
+
 }  // namespace
 
 int main()
 {
-  // TODO: have some main function that takes std::tuple<std::tuple<Ts...>, ...>
-  // so we can know how many inputs are being sent. also may need an input type
-  unsigned int failed = 0;
-  // 1.
+  // input types
   using types_1 = std::tuple<int, short, npygl::py_optional_args, PyObject*>;
-  check_format<types_1>(failed, "ih|O");
-  // 2.
   using types_2 = std::tuple<
     double, float, PyObject*,
     npygl::py_optional_args, Py_ssize_t, const char*, Py_complex
   >;
-  check_format<types_2>(failed, "dfO|nsD");
-  // 3.
   using types_3 = std::tuple<
     const char*, const char*, PyObject*,
     npygl::py_optional_args, Py_complex, Py_complex, int, Py_ssize_t
   >;
-  check_format<types_3>(failed, "ssO|DDin");
-  // all the input types + number of tests in total
-  using all_types = std::tuple<types_1, types_2, types_3>;
-  constexpr auto n_tests = std::tuple_size_v<all_types>;
-  // report failure/success
-  if (failed)
-    std::cout << "Failed: " << failed << " of " << n_tests << std::endl;
-  else
-    std::cout << "Passed " << n_tests << " of " << n_tests << std::endl;
-  return !!failed;
+  using types_4 = std::tuple<
+    Py_ssize_t, PyBytesObject*,
+    npygl::py_optional_args, double, Py_complex, PyObject*
+  >;
+  // run tests
+  return test_main(
+    make_input<types_1>("ih|O"),
+    make_input<types_2>("dfO|nsD"),
+    make_input<types_3>("ssO|DDin"),
+    make_input<types_4>("nS|dDO")
+  );
 }
