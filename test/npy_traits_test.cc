e54b09e0b18bd04337b4a9dff1b6890ba4ae7349
Derek Huang <djh458@stern.nyu.edu>
2024-10-09 01:20:06 -0400

    npy_traits_test.cc: stuff all the input types into the driver_type type alias

--- test/npy_traits_test.cc
+++ test/npy_traits_test.cc
@@ -25,55 +25,59 @@
 
 namespace {
 
-// has_npy_type_traits test inputs
-using input_types_1 = std::tuple<
-  double,
-  float,
-  int,
-  unsigned,
-  long,
-  unsigned long,
-  std::complex<double>,
-  std::complex<float>,
-  std::pair<std::string, std::false_type>,
-  std::pair<std::map<std::string, double>, std::false_type>,
-  std::pair<std::pair<std::pair<int, int>, std::string>, std::false_type>
->;
-// can_make_ndarray test inputs
-using input_types_2 = std::tuple<
-  std::vector<double>,
-  std::pair<std::vector<float>, std::true_type>,
-  std::pmr::vector<unsigned>,
-  std::pmr::vector<double>,
-  std::pair<std::map<unsigned, std::vector<double>>, std::false_type>,
+// test driver type
+using driver_type = npygl::testing::traits_checker_driver<
+  // has_npy_type_traits
+  npygl::testing::traits_checker<
+    npygl::has_npy_type_traits,
+    std::tuple<
+      double,
+      float,
+      int,
+      unsigned,
+      long,
+      unsigned long,
+      std::complex<double>,
+      std::complex<float>,
+      std::pair<std::string, std::false_type>,
+      std::pair<std::map<std::string, double>, std::false_type>,
+      std::pair<std::pair<std::pair<int, int>, std::string>, std::false_type>
+    >
+  >,
+  // can_make_ndarray
+  npygl::testing::traits_checker<
+    npygl::can_make_ndarray,
+    std::tuple<
+      std::vector<double>,
+      std::pair<std::vector<float>, std::true_type>,
+      std::pmr::vector<unsigned>,
+      std::pmr::vector<double>,
+      std::pair<std::map<unsigned, std::vector<double>>, std::false_type>,
 #if NPYGL_HAS_EIGEN3
-  Eigen::MatrixXd,
-  Eigen::MatrixXf,
-  Eigen::Matrix4cd,
+      Eigen::MatrixXd,
+      Eigen::MatrixXf,
+      Eigen::Matrix4cd,
 #endif  // NPYGL_HAS_EIGEN3
 #if NPYGL_HAS_ARMADILLO
-  arma::mat,
-  arma::fmat,
-  arma::cube,
-  arma::rowvec,
-  arma::fvec,
+      arma::mat,
+      arma::fmat,
+      arma::cube,
+      arma::rowvec,
+      arma::fvec,
 #endif  // NPYGL_HAS_ARMADILLO
-  std::pair<std::vector<std::vector<double>>, std::false_type>,
-  std::pair<
-    std::tuple<std::pair<unsigned, std::true_type>, std::pair<double, int>>,
-    std::false_type
+      std::pair<std::vector<std::vector<double>>, std::false_type>,
+      std::pair<
+        std::tuple<std::pair<unsigned, std::true_type>, std::pair<double, int>>,
+        std::false_type
+      >
+    >
   >
->;
-// test driver type
-using test_driver_type = npygl::testing::traits_checker_driver<
-  npygl::testing::traits_checker<npygl::has_npy_type_traits, input_types_1>,
-  npygl::testing::traits_checker<npygl::can_make_ndarray, input_types_2>
 >;
 
 }  // namespace
 
 int main()
 {
-  test_driver_type driver;
+  driver_type driver;
   return driver() ? EXIT_SUCCESS : EXIT_FAILURE;
 }
