9b27349373733eea65187f7c1b5b7d063b907749
Derek Huang <djh458@stern.nyu.edu>
2024-11-24 14:14:37 -0500

    npy_capsule_test.cc: refactor to use make_ndarray_test(), add helper traits to constrain template

--- test/npy_capsule_test.cc
+++ test/npy_capsule_test.cc
@@ -10,6 +10,9 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <typeinfo>
+#include <type_traits>
+#include <utility>
 #include <vector>
 
 #include "npygl/features.h"
@@ -28,6 +31,83 @@
 
 // TODO: consider having this not be a giant main() function
 
+namespace {
+
+// TODO: document; also unnecessary since we can directly evaluate the lambda
+template <typename T, typename = void>
+struct produces_ndarray_convertible : std::false_type {};
+
+template <typename T>
+struct produces_ndarray_convertible<
+  T,
+  std::enable_if_t<npygl::can_make_ndarray_v<std::invoke_result_t<T>>>
+> : std::true_type {};
+
+/**
+ * SFINAE helper to indicate if a type can be used to create a NumPy array.
+ *
+ * @note May want to move to `ndarray.h` to improve the type traits there.
+ *
+ * @tparam T type
+ */
+template <typename T>
+using make_ndarray_test_t = std::enable_if_t<
+  // require move-only construction + type must have a builder
+  (!std::is_reference_v<T> && npygl::can_make_ndarray_v<T>) ||
+  // or is invokable and return value type has a builder
+  produces_ndarray_convertible<T>::value
+>;
+
+/**
+ * Create a NumPy array from a C++ object and print the capsule and the array.
+ *
+ * If there is a Python error then `npygl::py_error_exit` is called.
+ *
+ * @tparam T type
+ *
+ * @param out Stream to write output to
+ * @param obj C++ object rvalue to create a NumPy array from
+ */
+template <typename T, typename = make_ndarray_test_t<T>>
+void make_ndarray_test(std::ostream& out, T&& obj)
+{
+  // create NumPy array backed by C++ object
+  auto ar = [&obj]
+  {
+    // if callable
+    if constexpr (produces_ndarray_convertible<T>::value)
+      return npygl::make_ndarray(obj());
+    // object is directly convertible
+    else
+      return npygl::make_ndarray(std::move(obj));
+  }();
+  npygl::py_error_exit();
+  // get backing base object for the object
+  // note: need template qualifier since ar implicitly depends on T
+  auto ar_base = PyArray_BASE(ar.template as<PyArrayObject>());
+  // write to stream with type name + base
+  out << "-- " << npygl::type_name(typeid(T)) << " (" << ar_base << ")\n" <<
+    ar << std::endl;
+}
+
+/**
+ * Create a NumPy array from a C++ object and print the capsule and the array.
+ *
+ * If there is a Python error then `npygl::py_error_exit` is called. This
+ * overload simply writes to `std::cout` as the output stream.
+ *
+ * @tparam T type
+ *
+ * @param obj C++ object rvalue to create a NumPy array from
+ */
+template <typename T, typename = make_ndarray_test_t<T>>
+void make_ndarray_test(T&& obj)
+{
+  make_ndarray_test(std::cout, std::move(obj));
+}
+
+}  // namespace
+
 int main()
 {
   // initialize Python + import NumPy API
@@ -36,22 +116,19 @@ int main()
   // print version
   std::cout << Py_GetVersion() << std::endl;
   // create NumPy arrays backed by double and integer vectors
-  auto d_ar = npygl::make_ndarray(std::vector{2.3, 1.222, 14.23, 3.243, 5.556});
-  npygl::py_error_exit();
-  auto i_ar = npygl::make_ndarray(std::vector{3, 14, 1, 555, 34, 3, 8, 42});
-  npygl::py_error_exit();
+  make_ndarray_test(std::vector{2.3, 1.222, 14.23, 3.243, 5.556});
+  make_ndarray_test(std::vector{3, 14, 1, 555, 34, 3, 8, 42});
 #if NPYGL_HAS_EIGEN3
   // create a NumPy array backed by an Eigen column-major matrix
-  auto e_ar = npygl::make_ndarray(
+  make_ndarray_test(
     Eigen::MatrixXf{
       {4.333f, 1.44f, 1.532f, 1.222f},
       {5.6634f, 2.2f, 1.555f, 5.64f},
       {6.7774f, 4.87f, 9.875f, 3.22f}
     }
   );
-  npygl::py_error_exit();
   // create a NumPy array backed by an Eigen row-major matrix
-  auto ec_ar = npygl::make_ndarray(
+  make_ndarray_test(
     Eigen::Matrix<
       unsigned int,
       Eigen::Dynamic,
@@ -63,113 +140,63 @@ int main()
       {31, 6, 44, 1, 23}
     }
   );
-  npygl::py_error_exit();
 #endif  // NPYGL_HAS_EIGEN3
 #if NPYGL_HAS_ARMADILLO
   // create a NumPy array backed by an Armadillo complex matrix
-  auto a_ar = npygl::make_ndarray(
+  make_ndarray_test(
     arma::cx_mat{
       {{3.44, 5.423}, {9.11, 4.333}, {4.63563, 1.111}},
       {{4.23, 2.123}, {3.4244, 5.22}, {0.999, 12.213}}
     }
   );
-  npygl::py_error_exit();
   // create a NumPy array backed by an Armadillo complex cube
-  arma::cx_cube cube{2, 2, 3};
-  cube.slice(0) = {
-    {{3.4, 2.22}, {3.22, 4.23}},
-    {{5.34, 5.111}, {6.66, 1.123}}
-  };
-  cube.slice(1) = {
-    {{6.455, 1.111}, {4.232, 0.989}},
-    {{6.1212, 1.1139}, {6.45, 0.2345}}
-  };
-  cube.slice(2) = {
-    {{1.12, 4.412}, {5.34, 6.111}},
-    {{4.123, 1.998}, {8.99, 1.114}}
-  };
-  auto ac_ar = npygl::make_ndarray(std::move(cube));
-  npygl::py_error_exit();
+  make_ndarray_test(
+    []
+    {
+      arma::cx_cube cube{2, 2, 3};
+      // need to set slice-by-slice
+      cube.slice(0) = {
+        {{3.4, 2.22}, {3.22, 4.23}},
+        {{5.34, 5.111}, {6.66, 1.123}}
+      };
+      cube.slice(1) = {
+        {{6.455, 1.111}, {4.232, 0.989}},
+        {{6.1212, 1.1139}, {6.45, 0.2345}}
+      };
+      cube.slice(2) = {
+        {{1.12, 4.412}, {5.34, 6.111}},
+        {{4.123, 1.998}, {8.99, 1.114}}
+      };
+      return cube;
+    }
+  );
   // create a NumPy array backed by an Armadillo float column vector
-  auto av_ar = npygl::make_ndarray(arma::fvec{1.f, 3.4f, 4.23f, 3.54f, 5.223f});
-  npygl::py_error_exit();
+  make_ndarray_test(arma::fvec{1.f, 3.4f, 4.23f, 3.54f, 5.223f});
   // create a NumPy array backed by an Armadillo double row vector
-  auto arv_ar = npygl::make_ndarray(arma::rowvec{5., 4.33, 2.433, 1.22, 4.34});
-  npygl::py_error_exit();
+  make_ndarray_test(arma::rowvec{5., 4.33, 2.433, 1.22, 4.34});
 #endif  // NPYGL_HAS_ARMADILLO
 #if NPYGL_HAS_LIBTORCH
-  // PyTorch generator object for reproducibility. no need to acquire impl
-  // mutex in single-threaded runtime like for this program
-  auto gen = at::make_generator<at::CPUGeneratorImpl>();
   // create a NumPy array backed by a random PyTorch float tensor
-  auto t_ar = npygl::make_ndarray(torch::randn({2, 3, 4}, gen));
-  npygl::py_error_exit();
+  make_ndarray_test(
+    []
+    {
+      // PyTorch generator object for reproducibility. no need to acquire impl
+      // mutex in single-threaded runtime like for this program
+      auto gen = at::make_generator<at::CPUGeneratorImpl>();
+      return torch::randn({2, 3, 4}, gen);
+    }()
+  );
 #endif  // NPYGL_HAS_LIBTORCH
   // create a "normal" NumPy array
+  // TODO: extend make_ndarray_test to allow custom type formatting
   auto ar_init = Py_BuildValue("ddddd", 3.4, 1.222, 6.745, 5.2, 5.66, 7.333);
   npygl::py_error_exit();
-  auto ar = npygl::make_ndarray<double>(ar_init);
+  // note: take ownership as we need to Py_DECREF the created object
+  auto ar = npygl::make_ndarray<double>(npygl::py_object{ar_init});
   npygl::py_error_exit();
-  // print the repr() for each array
-  std::cout <<
-    "NumPy arrays\n"
-    "============\n\n"
-    "-- std::vector<double>\n" << d_ar << '\n' <<
-    "-- std::vector<int>\n" << i_ar << '\n' <<
-#if NPYGL_HAS_EIGEN3
-    "-- Eigen::MatrixXf\n" << e_ar << '\n' <<
-    "-- Eigen::Matrix<unsigned, Eigen::Dynamic, Eigen::Dynamic, "
-      "Eiden::StorageOptions::RowMajor>\n" << ec_ar << '\n' <<
-#endif  // NPYGL_HAS_EIGEN3
-#if NPYGL_HAS_ARMADILLO
-    "-- arma::cx_mat\n" << a_ar << '\n' <<
-    "-- arma::cx_cube\n" << ac_ar << '\n' <<
-    "-- arma::fvec\n" << av_ar << '\n' <<
-    "-- arma::rowvec\n" << arv_ar << '\n' <<
-#endif  // NPYGL_HAS_ARMADILLO
-#if NPYGL_HAS_LIBTORCH
-    "-- torch::Tensor\n" << t_ar << '\n' <<
-#endif  // NPYGL_HAS_LIBTORCH
-    "-- tuple[double]\n" << ar << '\n' << std::endl;
-  npygl::py_error_exit();
-  // get base objects for each array
-  auto d_base = PyArray_BASE(d_ar.as<PyArrayObject>());
-  auto i_base = PyArray_BASE(i_ar.as<PyArrayObject>());
-#if NPYGL_HAS_EIGEN3
-  auto e_base = PyArray_BASE(e_ar.as<PyArrayObject>());
-  auto ec_base = PyArray_BASE(ec_ar.as<PyArrayObject>());
-#endif  // NPYGL_HAS_EIGEN3
-#if NPYGL_HAS_ARMADILLO
-  auto a_base = PyArray_BASE(a_ar.as<PyArrayObject>());
-  auto ac_base = PyArray_BASE(ac_ar.as<PyArrayObject>());
-  auto av_base = PyArray_BASE(av_ar.as<PyArrayObject>());
-  auto arv_base = PyArray_BASE(arv_ar.as<PyArrayObject>());
-#endif  // NPYGL_HAS_ARMADILLO
-#if NPYGL_HAS_LIBTORCH
-  auto t_base = PyArray_BASE(t_ar.as<PyArrayObject>());
-#endif  // NPYGL_HAS_LIBTORCH
+  // get base object
   auto base = PyArray_BASE(ar.as<PyArrayObject>());
-  // print base objects for each array
-  std::cout <<
-    "NumPy array bases\n" <<
-    "=================\n\n" <<
-    "-- std::vector<double>\n" << d_base << '\n' <<
-    "-- std::vector<int>\n" << i_base << '\n' <<
-#if NPYGL_HAS_EIGEN3
-    "-- Eigen::MatrixXf\n" << e_base << '\n' <<
-    "-- Eigen::Matrix<unsigned int, Eigen::Dynamic, Eigen::Dynamic, "
-      "Eigen::StorageOptions::RowMajor>\n" << ec_base << '\n' <<
-#endif  // NPYGL_HAS_EIGEN3
-#if NPYGL_HAS_ARMADILLO
-    "-- arma::cx_mat\n" << a_base << '\n' <<
-    "-- arma::cx_cube\n" << ac_base << '\n' <<
-    "-- arma::fvec\n" << av_base << '\n' <<
-    "-- arma::rowvec\n" << arv_base << '\n' <<
-#endif  // NPYGL_HAS_ARMADILLO
-#if NPYGL_HAS_LIBTORCH
-    "-- torch::Tensor\n" << t_base << '\n' <<
-#endif  // NPYGL_HAS_LIBTORCH
-    "-- tuple[double]\n" << base << std::endl;
-  npygl::py_error_exit();
+  // print the repr()
+  std::cout << "-- tuple[double] (" << base << ")\n" << ar << std::endl;
   return EXIT_SUCCESS;
 }
