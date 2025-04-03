fce8a9677184cebc70e02ef0e67ef7c1b45c6edb
Derek Huang <djh458@stern.nyu.edu>
2025-04-03 00:47:14 -0400

    add npygl_llvm_library_target to use LLVM libraries in target fashion
    modified:   CMakeLists.txt
        Append to CMAKE_MODULE_PATH instead of overriding and use
        npygl_llvm_library_target to add a npygl::llvm_demangle target
    
    new file:   cmake/npygl_llvm_library_target.cmake
        Add function to create imported INTERFACE target for an LLVM library
    
    modified:   test/CMakeLists.txt
        Use npygl::llvm_demangle target instead of library name

---
+++ cmake/npygl_llvm_library_target.cmake
@@ -0,0 +1,28 @@
+cmake_minimum_required(VERSION 3.15)
+
+##
+# npygl_llvm_library_target.cmake
+#
+# Provides a function to create an LLVM target from the given library name.
+#
+
+##
+# Create a target for the specified LLVM component library.
+#
+# The created INTERFACE imported target will be npygl::llvm_<component>. It is
+# assumed that LLVM was found with find_package.
+#
+# Arguments:
+#   component       LLVM component to create target from
+#
+function(npygl_llvm_library_target component)
+    # resolve LLVM component
+    llvm_map_components_to_libnames(libname ${component})
+    # add target + LLVM include directories
+    add_library(npygl::llvm_${component} INTERFACE IMPORTED)
+    target_include_directories(
+        npygl::llvm_${component}
+        SYSTEM INTERFACE ${LLVM_INCLUDE_DIRS}
+    )
+    target_link_libraries(npygl::llvm_${component} INTERFACE ${libname})
+endfunction()
