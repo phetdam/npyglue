11ac432af4f80449873c9a3405e7f12abea4344b
Derek Huang <djh458@stern.nyu.edu>
2025-04-08 22:46:42 -0400

    add py_capsule_cutorch_test to test GPU-back PyTorch tensor capsule
    modified:   CMakeLists.txt
        Ensure compiler flags are prefixed with -Xcompiler for nvcc, add more
        CUDA-specific warnings that were seen with nvcc, use /W4 for CUDA
    
    modified:   test/CMakeLists.txt
        Add py_capsule_cutorch_test target using npygl_add_py3_executable + test
    
    new file:   test/py_capsule_cutorch_test.cu
        Add simple program to start the Python interpreter and create a capsule
        from a CUDA GPU-backed Torch tensor

---
+++ test/py_capsule_cutorch_test.cu
@@ -0,0 +1,48 @@
+/**
+ * @file py_capsule_cutorch_test.cu
+ * @author Derek Huabg
+ * @brief CUDA C++ program to test PyCapsule creation from GPU-backed objects
+ * @copyright MIT License
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <typeinfo>  // for well-formed typeid usage
+#include <utility>
+
+#include <ATen/core/Generator.h>
+#include <ATen/cuda/CUDAGeneratorImpl.h>  // for at::CUDAGeneratorImpl
+#include <torch/torch.h>
+
+#include "npygl/demangle.hh"
+#include "npygl/ostream.hh"
+#include "npygl/python.hh"
+
+int main()
+{
+  // init Python + print version
+  npygl::py_init();
+  std::cout << Py_GetVersion() << std::endl;
+  // create CUDA generator + tensor
+  auto gen = at::make_generator<at::CUDAGeneratorImpl>();
+  auto ten = torch::randn({3, 4, 5}, gen, torch::kCUDA);
+  // note: following is copied from capsule_test in py_capsule_test.cc
+  // wrapped stream
+  npygl::ostream_wrapper stream{std::cout};
+  // create capsule from the moved object
+  auto cap = npygl::py_object::create(std::move(ten));
+  npygl::py_error_exit();
+  // take capsule view
+  npygl::cc_capsule_view view{cap};
+  npygl::py_error_exit();
+  // print the type name
+  const auto& val = *view.as<decltype(ten)>();
+  stream << "-- " << npygl::type_name(view.info()) << '\n';
+  // write the object to the output stream + flush
+  stream << val << std::endl;
+  // create copy via addition + print
+  auto val2 = val + val;
+  stream << "-- " << npygl::type_name(typeid(val2)) << '\n';
+  stream << val2 << std::endl;
+  return EXIT_SUCCESS;
+}
