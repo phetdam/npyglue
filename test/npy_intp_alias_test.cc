aaf94a0d25fbbf0c29315044244207512b1a9be5
Derek Huang <djh458@stern.nyu.edu>
2025-04-11 21:24:05 -0400

    add test to check that npy_intp aliasing works as expected
    modified:   test/CMakeLists.txt
        Add test for npy_intp_alias_test
    
    new file:   test/npy_intp_alias_test.cc
        Add test to determine if the Torch dim/stride type (int64_t) is
        type-accessible as a npy_intp (check strict aliasing adherence)

---
+++ test/npy_intp_alias_test.cc
@@ -0,0 +1,39 @@
+/**
+ * @file npy_intp_alias_test.cc
+ * @author Derek Huang
+ * @brief C++ program to verify when npy_intp can alias an `at::IntArrayRef`
+ * @copyright MIT License
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+#include <ATen/core/TensorBase.h>  // for at::TensorBase::size
+#include <numpy/npy_common.h>  // for npy_intp
+
+#include "npygl/type_traits.hh"
+
+namespace {
+
+// should be int64_t
+using torch_dim_type = decltype(std::declval<at::TensorBase>().size(0));
+// test result
+constexpr bool aliasable = npygl::is_accessible_as_v<torch_dim_type, npy_intp>;
+// message components
+constexpr auto status = (aliasable) ? "  OK" : "FAIL";
+constexpr auto maybe_neg = (aliasable) ? "" : "NOT ";
+
+}  // namespace
+
+int main()
+{
+  // Torch tensor stride type is same size as npy_intp
+  if constexpr (sizeof(torch_dim_type) == sizeof(npy_intp))
+    std::cout << status << ": torch_dim_type " << maybe_neg <<
+      "accessible as npy_intp" << std::endl;
+  // skip
+  else
+    std::cout << "SKIPPED: sizeof(torch_dim_type) != sizeof(npy_intp)" <<
+      std::endl;
+  return EXIT_SUCCESS;
+}
