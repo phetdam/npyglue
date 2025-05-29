78825c535feb991b4c45fd04b914e5abe2c27f17
Derek Huang <djh458@stern.nyu.edu>
2025-05-29 04:03:21 -0400

    traits_checker_filter_test.cc: remove unused operator()(std::ostream&, int, char**) const, implement operator()(int, char**) const, and use 9in main()

--- test/traits_checker_filter_test.cc
+++ test/traits_checker_filter_test.cc
@@ -8,6 +8,7 @@
 #include <cstdlib>
 #include <functional>
 #include <iostream>
+#include <regex>
 #include <sstream>
 #include <string>
 #include <string_view>
@@ -54,6 +55,9 @@ using driver_type = npygl::testing::traits_checker_driver<
  * where `Ts` might be one type and so `Traits` and `T` are available.
  */
 struct filter_test_driver : public driver_type {
+  // prevent shadowing
+  using driver_type::operator();
+
   /**
    * Runtime test map for test filtering.
    *
@@ -69,20 +73,8 @@ struct filter_test_driver : public driver_type {
   /**
    * Run the traits checker test suite and indicate success or failure.
    *
-   * This overload takes arguments from `main()` to allow test filtering.
-   *
-   * @param out Stream to write messages to
-   * @param argc Argument count from `main()`
-   * @param argv Argument vector from `main()`
-   * @return `true` on success, `false` on failure
-   */
-  bool operator()(std::ostream& out, int argc, char** argv) const;
-
-  /**
-   * Run the traits checker test suite and indicate success or failure.
-   *
    * This overload takes arguments from `main()` to allow test filtering and
-   * writes all its output to standard output.
+   * should be used in `main()` to run the entire test program.
    *
    * @param argc Argument count from `main()`
    * @param argv Argument vector from `main()`
@@ -194,22 +186,141 @@ struct traits_checker_driver_runtime_mapper<
   }
 };
 
+////////////////////////////////////////////////////////////////////////////////
+// operator() implementation for the filter_test_driver                       //
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Command-line options accepted by the `traits_checker_driver<Ts...>`.
+ *
+ * @param print_usage Print the driver help
+ * @param list_tests List all the test cases to be run
+ * @param test_pattern Test pattern to evaluate (run in alphabetical order)
+ * @param print_driver_type Print the `traits_checker_driver<Ts...>` type
+ */
+struct traits_checker_options {
+  // TODO: use action enum instead of so many boolean flags
+  bool print_usage = false;
+  bool list_tests = false;
+  std::string test_pattern;
+  bool print_driver_type = false;
+};
+
+/**
+ * Parse command-line options for the `traits_checker_driver<Ts...>`.
+ *
+ * @param opts Options to populate
+ * @param argc Argument count from `main()`
+ * @param argv Argument vector from `main()`
+ * @returns `true` if parsing succeeded, `false` otherwise
+ */
+bool parse_args(traits_checker_options& opts, int argc, char** argv)
+{
+  for (int i = 1; i < argc; i++) {
+    // use string_view for convenience
+    std::string_view arg{argv[i]};
+    // -h, --help. short circuit if printing help
+    if (arg == "-h" || arg == "--help") {
+      opts.print_usage = true;
+      return true;
+    }
+    // -l, --list-tests
+    else if (arg == "-l" || arg == "--list-tests")
+      opts.list_tests = true;
+    // -t, --test-pattern
+    else if (arg == "-t" || arg == "--test-pattern") {
+      // no argument
+      if (++i == argc) {
+        std::cerr << "Error: No argument provided for -t, --test-pattern" <<
+          std::endl;
+        return false;
+      }
+      // use test pattern
+      opts.test_pattern = argv[i];
+    }
+    // --print-driver-type
+    else if (arg == "--print-driver-type")
+      opts.print_driver_type = true;
+    // unknown option
+    else {
+      std::cerr << "Error: Unknown option \"" << arg <<
+        "\". Try -h, --help for usage" << std::endl;
+      return false;
+    }
+  }
+  // flags are nutually exclusive
+  if (opts.list_tests + opts.print_driver_type + !opts.test_pattern.empty() > 1) {
+    std::cerr << "Error: Only one of  -l, --list-tests, --print-driver-type, "
+      " -t, --test-pattern may be specified" << std::endl;
+    return false;
+  }
+  // done
+  return true;
+}
+
+// TODO: organize
+bool
+filter_test_driver::operator()(int argc, char** argv) const
+{
+  // parse incoming args
+  traits_checker_options opts;
+  if (!parse_args(opts, argc, argv))
+    return false;
+  // print usage
+  // TODO: stub for now
+  if (opts.print_usage) {
+    std::cout <<
+      "Usage: <progname> [-h] [-l | -t TEST_PATTERN | --print-driver-type]" <<
+      std::endl;
+    return true;
+  }
+  // test mapper type alias
+  // note: cheap to create as it had no data members
+  // TODO: allow subclasses of the traits_checker_driver<Ts...>?
+  traits_checker_driver_runtime_mapper<driver_type> test_mapper;
+  // list tests
+  if (opts.list_tests) {
+    for (const auto& [name, _] : test_mapper())
+      std::cout << name << '\n';
+    // flush and return
+    std::cout << std::flush;
+    return true;
+  }
+  // print driver type
+  // TODO: use type_name<T>() instead? abi::__cxa_demangle fails on long types
+  if (opts.print_driver_type) {
+    std::cout << npygl::type_name(typeid(driver_type)) << std::endl;
+    return true;
+  }
+  // run test patterns
+  if (opts.test_pattern.size()) {
+    // regex pattern for search
+    std::regex pattern{opts.test_pattern};
+    // popluate vector of tests to run
+    std::vector<typename test_mapping::mapped_type> tests;
+    for (const auto& [name, test] : test_mapper())
+      if (std::regex_search(name, pattern))
+        tests.push_back(test);
+    // execute selected tests
+    auto n_failed = 0u;
+    for (const auto& test : tests)
+      n_failed += !test();
+    // done
+    return !n_failed;
+  }
+  // no special options so run all test cases
+  return operator()();
+}
+
+// experimenal test driver
+constexpr filter_test_driver driver;
+
 }  // namespace
 
-int main()
+int main(int argc, char** argv)
 {
   // note: no-op on POSIX systems and unnecessary in Windows Terminal
   npygl::vts_stdout_context ctx;
-  // test driver runtime test mapping
-  traits_checker_driver_runtime_mapper<driver_type> mapper;
-  auto tests = mapper();
-  // TODO: allow listing tests + executing specific tests
-  // print test names
-  for (const auto& [name, _] : tests)
-    std::cout << name << std::endl;
-  // execute + count failed
-  auto n_failed = 0u;
-  for (const auto& [_, test] : tests)
-    n_failed += !test();
-  return (n_failed) ? EXIT_FAILURE : EXIT_SUCCESS;
+  // run with command-line options
+  return driver(argc, argv);
 }
