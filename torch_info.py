f46984640aa4efdf54816b8d9c97eccb0fb050eb
Derek Huang <djh458@stern.nyu.edu>
2025-07-01 21:51:26 -0400

    torch_info.py: remove build info from version

--- torch_info.py
+++ torch_info.py
@@ -3,7 +3,7 @@
 Since PyTorch takes several seconds to load, especially on Windows, we
 collect several pieces of configuration information at once:
 
-    * The PyTorch major.minor.patch[+flavor] version
+    * The PyTorch major.minor.patch version
     * The PyTorch flavor, e.g. cpu, xpu, cu124, etc.
     * The PyTorch CMake config script prefix
     * The PyTorch CUDA version if any
@@ -17,8 +17,8 @@ from torch import utils, version
 if __name__ == "__main__":
     # split version into major.minor.patch[+flavor]
     ver_cmps = version.__version__.split("+")
-    # print full version
-    print(version.__version__)
+    # print major.minor.patch version
+    print(ver_cmps[0])
     # print flavor, defaulting to CPU if no flavor
     print("cpu" if len(ver_cmps) == 1 else ver_cmps[-1])
     # CMake prefix to look for TorchConfig.cmake (not the install root)
