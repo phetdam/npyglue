de8373f0be504e2123fcf482a6069c3404482cec
Derek Huang <djh458@stern.nyu.edu>
2026-02-24 23:12:29 -0500

    string.hh: enable fixed_string ctor to take other fixed_string instances + add operator+ for concatenation

--- include/npygl/string.hh
+++ include/npygl/string.hh
@@ -32,6 +32,58 @@ constexpr auto strlen(const char* str) noexcept
   return n;
 }
 
+// forward decl for traits
+template <std::size_t N>
+class fixed_string;
+
+namespace detail {
+
+/**
+ * Traits type for `fixed_string<N>` ctor inputs providing the input size.
+ *
+ * @tparam T type
+ */
+template <typename T>
+struct fixed_string_input_size {};
+
+/**
+ * Partial specialization for a character array.
+ *
+ * The reported size is `N - 1` to exclude the null terminator.
+ *
+ * @tparam N Number of elements in array
+ */
+template <std::size_t N>
+struct fixed_string_input_size<char[N]> {
+  static constexpr auto value = N - 1u;
+};
+
+/**
+ * Partial specialization for a single `fixed_string<N>`.
+ *
+ * @tparam N String length excluding null terminator
+ */
+template <std::size_t N>
+struct fixed_string_input_size<fixed_string<N>> {
+  static constexpr auto value = N;
+};
+
+}  // namespace detail
+
+/**
+ * Helper to obtain the size of the `fixed_string<N>` input.
+ *
+ * @note Originally this was defined using a fold expression and took a type
+ *  pack but in C++17 MSVC ended up emitting C7516 (unary fold expression over
+ *  + must have a non-empty expansion). Therefore we stick with using a single
+ *  type parameter and require explicit folding over type packs.
+ *
+ * @tparam T Input type with cv-ref qualifiers
+ */
+template <typename T>
+constexpr auto fixed_string_input_size_v = detail::
+  fixed_string_input_size<std::remove_cv_t<std::remove_reference_t<T>>>::value;
+
 /**
  * Class template for a fixed-length string.
  *
@@ -54,22 +106,25 @@ public:
   /**
    * Ctor.
    *
-   * This enables construction from multiple string literals.
+   * Constructs from multiple string literals or `fixed_string<N>`.
    *
-   * @tparam Ns Null-terminated character array sizes
+   * @tparam Ts Input types
    *
-   * @param strs String literals to construct from
+   * @param inputs String literals of `fixed_string<N>` inputs
    */
-  template <std::size_t... Ns, typename = std::enable_if_t<N == ((Ns - 1) + ...)> >
-  constexpr fixed_string(const char (&...strs)[Ns]) noexcept
+  template <
+    typename... Ts,
+    typename = std::enable_if_t<N == (fixed_string_input_size_v<Ts> + ...)> >
+  constexpr fixed_string(Ts&&... inputs) noexcept
   {
+    // current position in data_
     decltype(N) count = 0u;
+    // copy characters from inputs using fold
     (
-      [this, &count, &strs]
+      [this, &count, &inputs]
       {
-        // copy characters except trailing null terminator
-        for (decltype(Ns) i = 0u; i < Ns - 1; i++)
-          data_[count++] = strs[i];
+        for (decltype(N) i = 0u; i < fixed_string_input_size_v<Ts>; i++)
+          data_[count++] = inputs[i];
       }(), ...
     );
   }
@@ -128,14 +183,60 @@ private:
 };
 
 /**
- * Deduction guide for CTAD when constructing from string literals.
+ * Deduction guide for CTAD when constructing from `fixed_string<N>` inputs.
  *
- * @tparam Ns Null-terminated character array sizes
- *
- * @param strs String literals to construct from
+ * @tparam Ts Input types
  */
-template <std::size_t... Ns>
-fixed_string(const char (&...strs)[Ns]) -> fixed_string<((Ns - 1) + ...)>;
+template <typename... Ts>
+fixed_string(Ts&&...) -> fixed_string<(fixed_string_input_size_v<Ts> + ...)>;
+
+/**
+ * Concatenate two `fixed_string<N>` instances into a new `fixed_string<N>`.
+ *
+ * @tparam N1 First length
+ * @tparam N2 Second length
+ *
+ * @param s1 First fixed string
+ * @param s2 Second fixed string
+ */
+template <std::size_t N1, std::size_t N2>
+constexpr auto
+operator+(const fixed_string<N1>& s1, const fixed_string<N2>& s2) noexcept
+{
+  return fixed_string{s1, s2};
+}
+
+/**
+ * Concatenate a `fixed_string<N>` and a string literal.
+ *
+ * @tparam N1 First length
+ * @tparam N2 Second length
+ *
+ * @param s1 Fixed string
+ * @param s2 String literal
+ */
+template <std::size_t N1, std::size_t N2>
+constexpr auto
+operator+(const fixed_string<N1>& s1, const char (&s2)[N2]) noexcept
+{
+  return fixed_string{s1, s2};
+}
+
+/**
+ * Concatenate a string literal and a `fixed_string<N>`.
+ *
+ * @tparam N1 First length
+ * @tparam N2 Second length
+ *
+ * @param s1 String literal
+ * @param s2 Fixed string
+ */
+template <std::size_t N1, std::size_t N2>
+constexpr auto
+operator+(const char (&s1)[N1], const fixed_string<N2>& s2) noexcept
+{
+  return fixed_string{s1, s2};
+}
 
 /**
  * Compare two `fixed_string<N>` for equality.
