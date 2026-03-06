d462fd16783c20dd92e98c8510b645ac0b7330ac
Derek Huang <djh458@stern.nyu.edu>
2026-03-06 00:18:58 -0500

    npy_views_test.cc: replace ndarray_matrix_view with ndarray_2d_view for testing + add test_frame

--- test/npy_views_test.cc
+++ test/npy_views_test.cc
@@ -14,6 +14,7 @@
 #include <ios>
 #include <iostream>
 
+#include "npygl/common.h"
 #include "npygl/features.h"
 #include "npygl/ndarray.hh"
 #include "npygl/python.hh"
@@ -35,6 +36,74 @@ npygl::py_object npy_random() noexcept
 }
 
 /**
+ * RAII manager to print a test header and footer and manage checks.
+ */
+class test_frame {
+public:
+  /**
+   * Ctor.
+   *
+   * @param header Frame header, typically `NPYGL_PRETTY_FUNCTION_NAME`
+   */
+  test_frame(const char* header) noexcept : header_{header}
+  {
+    std::cout << "[ START ] " << header_ << ": " << std::endl;
+  }
+
+  /**
+   * Dtor.
+   */
+  ~test_frame()
+  {
+    if (failed_)
+      std::cout << "[ FAIL  ] " << header_ << ": failed " << failed_ <<
+        " of " << total_tests() << std::endl;
+    else
+      std::cout << "[ OK    ] " << header_ << ": passed " << passed_ <<
+        " of " << total_tests() << std::endl;
+  }
+
+  /**
+   * Indicate an abort occurred.
+   *
+   * @param text Abort message text
+   * @returns `false`
+   */
+  bool abort(const char* text)
+  {
+    failed_++;
+    std::cout << "[ ABORT ] " << text << std::endl;
+    return false;
+  }
+
+  /**
+   * Indicate a test success.
+   *
+   * @param text Success message text
+   * @returns `true`
+   */
+  bool success(const char* text)
+  {
+    passed_++;
+    std::cout << "[ PASS  ] " << text << std::endl;
+    return true;
+  }
+
+  /**
+   * Total number of checks.
+   */
+  unsigned total_tests() const noexcept
+  {
+    return passed_ + failed_;
+  }
+
+private:
+  const char* header_;
+  unsigned passed_{};
+  unsigned failed_{};
+};
+
+/**
  * Test modification of a NumPy array through flat views.
  *
  * @param np_random Python object representing `numpy.random.random`
@@ -42,13 +111,14 @@ npygl::py_object npy_random() noexcept
  */
 bool test_flat_views(PyObject* np_random)
 {
+  test_frame frame{NPYGL_PRETTY_FUNCTION_NAME};
   // create a random 3x4x2 tensor
   npygl::py_object shape{Py_BuildValue("(iii)", 3, 4, 2)};
   if (!shape)
-    return false;
+    return frame.abort("failed to create tuple (3, 4, 2)");
   auto obj = npygl::py_call_one(np_random, shape);
   if (!obj)
-    return false;
+    return frame.abort("failed to call np.random.random((3, 4, 2))");
   // sine + inverse sine functors
   auto sine = [](auto& v) { v = std::sin(v); };
   auto asine = [](auto& v) { v = std::asin(v); };
@@ -68,24 +138,25 @@ bool test_flat_views(PyObject* np_random)
   std::ranges::for_each(stl_view, asine);
   std::cout << "span inverse sine transform:\n" << ar << std::endl;
 #endif  // NPYGL_HAS_CC_20
-  return true;
+  return frame.success("passed trig transform tests");
 }
 
 /**
- * Test modification of a NumPy array through matrix views.
+ * Test modification of a NumPy array through 2D views.
  *
  * @param np_random Python object representing `numpy.random.random`
  * @returns `true` on success, `false` on failure
  */
-bool test_matrix_views(PyObject* np_random)
+bool test_2d_views(PyObject* np_random)
 {
+  test_frame frame{NPYGL_PRETTY_FUNCTION_NAME};
   // create a random 4x4 matrix
   npygl::py_object shape{Py_BuildValue("ii", 4, 4)};
   if (!shape)
-    return false;
+    return frame.abort("failed to create tuple (4, 4)");
   auto mat = npygl::py_call_one(np_random, shape);
   if (!mat)
-    return false;
+    return frame.abort("failed to call np.random.random((4, 4))");
   // print + enable boolalpha
   std::cout << mat << std::boolalpha << std::endl;
   // check that this is actually a 2D ndarray
@@ -93,7 +164,7 @@ bool test_matrix_views(PyObject* np_random)
   std::cout << "Is matrix? " << (PyArray_NDIM(mat_ar) == 2) << std::endl;
   // multiply diagonal by 2 and then halve diagonal using matrix view
   // note: this assumes C data ordering and double data type (the default)
-  npygl::ndarray_matrix_view<double> mat_view{mat_ar};
+  npygl::ndarray_2d_view<double> mat_view{mat_ar};
   for (unsigned int i = 0; i < mat_view.rows(); i++)
     mat_view(i, i) *= 2;
   std::cout << "doubled diagonal:\n" << mat_ar << std::endl;
@@ -109,7 +180,7 @@ bool test_matrix_views(PyObject* np_random)
     for (unsigned int j = i; j < mat_view.cols(); j++)
       mat_view(i, j) /= 2;
   std::cout << "halved upper triangle:\n" << mat_ar << std::endl;
-  return true;
+  return frame.success("passed transform tests");
 }
 
 }  // namespace
@@ -126,7 +197,7 @@ int main()
   auto np_random = npy_random();
   npygl::py_error_exit();
   // call test functions
-  if (!test_flat_views(np_random) || !test_matrix_views(np_random))
+  if (!test_flat_views(np_random) || !test_2d_views(np_random))
     return EXIT_FAILURE;
   return EXIT_SUCCESS;
 }
